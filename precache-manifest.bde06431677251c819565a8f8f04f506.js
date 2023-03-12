6e3fa15a1ec9fd47b13d6706ca673865da7dda47
soider2008 <313152514@qq.com>
2023-03-12 13:54:26 +0800

    新建fastst_web的测试环境

---
+++ precache-manifest.bde06431677251c819565a8f8f04f506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7326ae10ec283f4e91a7cb97ae079de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2e181fd972d9de5cd6c",
+    "url": "/static/css/2.e54e2d2f.chunk.css"
+  },
+  {
+    "revision": "a2e181fd972d9de5cd6c",
+    "url": "/static/js/2.f6cb7f51.chunk.js"
+  },
+  {
+    "revision": "79b2dbc1485b7669076b5ee34a29a98a",
+    "url": "/static/js/2.f6cb7f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2340baf40a95c35df36",
+    "url": "/static/js/main.7a68b4d0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7a68b4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1418eeaa711f9600190e",
+    "url": "/static/js/runtime-main.8bfec437.js"
+  }
+]);
